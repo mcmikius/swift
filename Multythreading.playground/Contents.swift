7f85da4d77833804d0122dac5c70798d8c4666c4
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 22:44:31 +0200

    global queue

--- Multythreading.playground/Contents.swift
+++ Multythreading.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
 var str = "Hello, playground"
+print("\(str)")
+
+DispatchQueue.global(qos: .userInitiated).async {
+    for i in 0...20 {
+        print("GlonalQueue \(i)")
+    }
+}
+
+print("end of file")
