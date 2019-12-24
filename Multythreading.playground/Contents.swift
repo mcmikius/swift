150fe06d2c2c9e389634aa6d5fa1eb2d0a242bc7
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 22:49:46 +0200

    sync

--- Multythreading.playground/Contents.swift
+++ Multythreading.playground/Contents.swift
@@ -3,16 +3,18 @@ import UIKit
 var str = "Hello, playground"
 print("\(str)")
 
-DispatchQueue.global(qos: .userInitiated).async {
+DispatchQueue.global(qos: .userInitiated).sync {
     for i in 0...100 {
         print("userInitiated \(i)")
     }
 }
 
-DispatchQueue.global(qos: .background).async {
+DispatchQueue.global(qos: .background).sync {
     for i in 0...100 {
         print("background \(i)")
     }
 }
+
+
 
 print("end of file")
