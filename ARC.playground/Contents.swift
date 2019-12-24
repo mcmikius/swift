7a9e24c48edcc4503375cd787f7342e484a2a12c
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 23:47:21 +0200

    closure

--- ARC.playground/Contents.swift
+++ ARC.playground/Contents.swift
@@ -1,25 +1,40 @@
 import UIKit
 
-class Human {
-    
-    var pet: Animal?
-    
-    deinit {
-        print("Human deinit")
-    }
-}
+//class Human {
+//
+//    var pet: Animal?
+//
+//    deinit {
+//        print("Human deinit")
+//    }
+//}
+//
+//class Animal {
+//
+//    weak var owner: Human?
+//    deinit {
+//        print("Animal deinit")
+//    }
+//}
+//
+//if true {
+//    var human = Human()
+//    var animal = Animal()
+//    human.pet = animal
+//    animal.owner = human
+//}
 
-class Animal {
-    
-    weak var owner: Human?
+class MyClass {
+    var number = 10
+    lazy var closure: (Int) -> Void = { [weak self] (number) in
+        self?.number += number
+    }
     deinit {
-        print("Animal deinit")
+        print("MyClass deinit")
     }
 }
 
 if true {
-    var human = Human()
-    var animal = Animal()
-    human.pet = animal
-    animal.owner = human
+    var x = MyClass()
+    x.closure(5)
 }
