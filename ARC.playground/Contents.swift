75aaf6ef6ce26a59ebe3cdce998fa7971afc2501
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 23:52:42 +0200

    weak closure

--- ARC.playground/Contents.swift
+++ ARC.playground/Contents.swift
@@ -26,8 +26,8 @@ import UIKit
 
 class MyClass {
     var number = 10
-    lazy var closure: (Int) -> Void = { (number) in
-        self.number += number
+    lazy var closure: (Int) -> Void = { [weak self] (number1) in
+        self?.number += number1
     }
     deinit {
         print("MyClass deinit")
