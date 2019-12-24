f3d4b5a173df7dec774e3dd2115ce2198f49e1a5
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 23:28:24 +0200

    animal

--- ARC.playground/Contents.swift
+++ ARC.playground/Contents.swift
@@ -1,11 +1,20 @@
 import UIKit
 
 class Human {
+    
     deinit {
         print("Human deinit")
     }
 }
 
+class Animal {
+    
+    deinit {
+        print("Animal deinit")
+    }
+}
+
 if true {
-    var x = Human()
+    var human = Human()
+    var animal = Animal()
 }
