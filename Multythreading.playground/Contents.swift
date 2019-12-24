d136ec78eb39ca06521d31e1c7a4ce83560fd9ba
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-24 22:54:31 +0200

    custom queue

--- Multythreading.playground/Contents.swift
+++ Multythreading.playground/Contents.swift
@@ -3,18 +3,26 @@ import UIKit
 var str = "Hello, playground"
 print("\(str)")
 
-DispatchQueue.global(qos: .userInitiated).sync {
-    for i in 0...100 {
-        print("userInitiated \(i)")
+//DispatchQueue.global(qos: .userInitiated).sync {
+//    for i in 0...100 {
+//        print("userInitiated \(i)")
+//    }
+//}
+//
+//DispatchQueue.global(qos: .background).sync {
+//    for i in 0...100 {
+//        print("background \(i)")
+//    }
+//}
+
+let myQueue = DispatchQueue(label: "mySerialQueue")
+for i in 0...100 {
+    myQueue.async {
+        print("A-\(i)")
+    }
+    myQueue.async {
+        print("B-\(i)")
     }
 }
-
-DispatchQueue.global(qos: .background).sync {
-    for i in 0...100 {
-        print("background \(i)")
-    }
-}
-
-
 
 print("end of file")
