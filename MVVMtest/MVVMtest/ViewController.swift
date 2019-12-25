8e218e1a5fb863525b1b513111d2e26d4ed17f06
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-25 19:58:19 +0200

    MVVM test init

---
+++ MVVMtest/MVVMtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMtest
+//
+//  Created by Mykhailo Bondarenko on 25.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
