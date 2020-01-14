3756f2e1e450d51cdf16d5347e1a184134844c30
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-14 15:27:36 +0200

    core data helper

--- Notes/Notes/ListNotesTableViewController.swift
+++ Notes/Notes/ListNotesTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ListNotesTableViewController: UITableViewController {
     
     // MARK: - Properties
-
+    
     var notes = [Note]() {
         didSet {
             tableView.reloadData()
@@ -20,83 +20,85 @@ class ListNotesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        notes = CoreDataHelper.retrieveNotes()
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return notes.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
-
+        
         let note = notes[indexPath.row]
         cell.noteTitleLabel.text = note.title
-        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
-
+        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString() ?? "unknown"
+        
         return cell
     }
-
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             notes.remove(at: indexPath.row)
         }
     }
-
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Check that the segue has a identifier.
         guard let identifier = segue.identifier else { return }
-
+        
         switch identifier {
         case "displayNote":
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
             let note = notes[indexPath.row]
             let destination = segue.destination as! DisplayNoteViewController
             destination.note = note
-
+            
         case "addNote":
             print("create note bar button item tapped")
-
+            
         default:
             print("unexpected segue identifier")
         }
@@ -105,8 +107,8 @@ class ListNotesTableViewController: UITableViewController {
     // MARK: - IBActions
     
     @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
-
+        notes = CoreDataHelper.retrieveNotes()
     }
-
-
+    
+    
 }
