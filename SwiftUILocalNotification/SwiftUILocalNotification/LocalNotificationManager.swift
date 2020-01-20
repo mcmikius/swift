c628e45c2e5d768bb0a42831edbc1dc036fd6d39
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-01-20 13:48:38 +0200

    Notification manager

---
+++ SwiftUILocalNotification/SwiftUILocalNotification/LocalNotificationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocalNotificationManager.swift
+//  SwiftUILocalNotification
+//
+//  Created by Mykhailo Bondarenko on 20.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class LocalNotificationManager {
+    var notifications = [Notification]()
+}
---
+++ SwiftUILocalNotification/SwiftUILocalNotification/Notification.swift
@@ -0,0 +1,14 @@
+//
+//  Notification.swift
+//  SwiftUILocalNotification
+//
+//  Created by Mykhailo Bondarenko on 20.01.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+struct Notification {
+    var id: String
+    var title: String
+}
